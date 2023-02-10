beba55be058d8c7b100d924bb4addfa44d7b80d4
bernard5 <bernard5@qq.com>
2023-02-10 23:17:43 +0800

    finish header part

---
+++ header/pool.go
@@ -0,0 +1,17 @@
+package header
+
+import "sync"
+
+var (
+	RequestPool  sync.Pool
+	ResponsePool sync.Pool
+)
+
+func init() {
+	RequestPool = sync.Pool{New: func() interface{} {
+		return &RequestHeader{}
+	}}
+	ResponsePool = sync.Pool{New: func() interface{} {
+		return &ResponseHeader{}
+	}}
+}

9febd43dc5bed8ad43fab9fa5db63a650efcaaf7
bernard5 <bernard5@qq.com>
2023-02-10 23:18:57 +0800

    finish client part

---
+++ client.go
@@ -0,0 +1,59 @@
+package LightRPC
+
+import (
+	"LightRPC/codec"
+	"LightRPC/compressor"
+	"LightRPC/serializer"
+	"io"
+	"net/rpc"
+)
+
+// Client rpc client based on net/rpc implementation
+type Client struct {
+	*rpc.Client
+}
+
+// Option provides options for rpc
+type Option func(o *options)
+
+type options struct {
+	compressType compressor.CompressType
+	serializer   serializer.Serializer
+}
+
+// WithCompress set client compression format
+func WithCompress(c compressor.CompressType) Option {
+	return func(o *options) {
+		o.compressType = c
+	}
+}
+
+// WithSerializer set client serializer
+func WithSerializer(serializer serializer.Serializer) Option {
+	return func(o *options) {
+		o.serializer = serializer
+	}
+}
+
+// NewClient Create a new rpc client
+func NewClient(conn io.ReadWriteCloser, opts ...Option) *Client {
+	options := options{
+		compressType: compressor.Raw,
+		serializer:   serializer.Proto,
+	}
+	for _, option := range opts {
+		option(&options)
+	}
+	return &Client{rpc.NewClientWithCodec(
+		codec.NewClientCodec(conn, options.compressType, options.serializer))}
+}
+
+// Call synchronously calls the rpc function
+func (c *Client) Call(serviceMethod string, args interface{}, reply interface{}) error {
+	return c.Client.Call(serviceMethod, args, reply)
+}
+
+// AsyncCall asynchronously calls the rpc function and returns a channel of *rpc.Call
+func (c *Client) AsyncCall(serviceMethod string, args interface{}, reply interface{}) chan *rpc.Call {
+	return c.Go(serviceMethod, args, reply, nil).Done
+}
